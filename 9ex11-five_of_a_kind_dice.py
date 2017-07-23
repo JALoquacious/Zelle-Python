bef3ef87613fa8573ebdfba7b24cf8e020e04833
JALoquacious <jalikoudis@protonmail.com>
2017-07-23 16:06:48 -0400

    Five-of-a-kind dice simulator initial commit

---
+++ 9ex11-five_of_a_kind_dice.py
@@ -0,0 +1,10 @@
+# Chapter 9 - Exercise 11
+# Write a program that performs a simulation to estimate the probability of
+# rolling five-of-a-kind in a single roll of five six-sided dice.
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
