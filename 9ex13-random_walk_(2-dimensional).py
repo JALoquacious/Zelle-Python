ded3f623def3863b0240054cb55c1bc56ca2a578
JALoquacious <jalikoudis@protonmail.com>
2017-07-26 09:54:39 -0400

    2-dimensional random walk initial commit

---
+++ 9ex13-random_walk_(2-dimensional).py
@@ -0,0 +1,14 @@
+# Chapter 9 - Exercise 13
+# Suppose you are doing a random walk (see previous problem) on the steps of a
+# city street. At each "step" you choose to walk one block (at random) either
+# forward, backward, left, or right. In n steps, how far do you expect to be
+# from the starting point? Write a program to help answer this question.
+
+from random import choice
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
