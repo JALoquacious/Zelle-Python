2e9591f7aec5eaecf50ff88203bec4dee9dea7f1
JALoquacious <jalikoudis@protonmail.com>
2017-07-26 11:39:05 -0400

    Graphical random walk initial commit

---
+++ 9ex14-random_walk-graphical.py
@@ -0,0 +1,20 @@
+# Chapter 9 - Exercise 14
+# Write a graphical program to trace a random walk (see previous two problems)
+# in two dimensions. In this simulation you should allow the step to be taken in
+# any direction. You can generate a random direction as an angle off of the x
+# axis.
+#   angle = random() * 2 * math.pi
+#
+# The new x and y positions are then given by these formulas:
+#   x = x + cos(angle)
+#   y = y + sin(angle)
+#
+# The program should take the number of steps as an input. Start your walker at
+# the center of a 100x100 grid and draw a line that traces the walk as it
+# progresses.
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
